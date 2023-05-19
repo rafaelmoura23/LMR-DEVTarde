e25a40d1515e6cf7ea65c3fe68ea4e44a31825a0
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-19 19:26:18 +0000

    19.05

--- AtividadeEvento/script.js
+++ AtividadeEvento/script.js
@@ -1,7 +1,19 @@
 function evento(){
-    let nPalestrante = document.getElementById("nValor1").value;
-    let nNomeEvento = document.getElementById("nValor2").value;
-    let nNumeroParticipantes = document.getElementById("nValor2").value;
-    let nData = document.getElementById("nValor2").value;
+    let nomePalestrante = document.getElementById("").value;
+    let nomeEvento = document.getElementById("").value;
+    let numeroParticipantes = document.getElementById("").value;
+    let data = document.getElementById("").value;
+
+    if(nomePalestrante==0){
+        alert("Digite o nome do Palestrante")
+    } else if (nomeEvento==0){
+        alert("Digite o nome do Evento")
+    } else if(numeroParticipantes<0){
+        alert("Digite o Número de Pessoas do evento")
+    } else if(numeroParticipantes>100){
+        alert("O Número de Participantes Excedeu o Limite(100)")
+    } else if (data){
+
+    }
 }
 
