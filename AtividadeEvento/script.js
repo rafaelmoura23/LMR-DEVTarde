cb00dd53516cdef65f6d61020248bd450a10e8ba
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-19 19:13:42 +0000

    19.05

---
+++ AtividadeEvento/script.js
@@ -0,0 +1,7 @@
+function evento(){
+    let nPalestrante = document.getElementById("nValor1").value;
+    let nNomeEvento = document.getElementById("nValor2").value;
+    let nNumeroParticipantes = document.getElementById("nValor2").value;
+    let nData = document.getElementById("nValor2").value;
+}
+
