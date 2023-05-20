1f4254214ae5b7c33210b0453e89d6403971b032
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-20 14:33:19 +0000

    20.05

--- AtividadeEvento/script.js
+++ AtividadeEvento/script.js
@@ -1,27 +1,23 @@
-function evento(){
+function evento() {
     let nomePalestrante = document.getElementById("pNome").value;
     let nomeEvento = document.getElementById("pEvento").value;
     let numeroParticipantes = document.getElementById("pParticipantes").value;
-    let dia = document.getElementById("pDia").value;
-    let mes = document.getElementById("pMes").value;
-    
 
-    if(nomePalestrante==0){
+
+    if (nomePalestrante == 0) {
         alert("Digite o nome do Palestrante")
-    } else if (nomeEvento==0){
+    } else if (nomeEvento == 0) {
         alert("Digite o nome do Evento")
-    } else if(numeroParticipantes<=0){
+    } else if (numeroParticipantes <= 0) {
         alert("Digite o Número de Participantes")
-    } else if(numeroParticipantes>100){
+    } else if (numeroParticipantes > 100) {
         alert("Excedido o número máximo de Participantes(100)")
-    } else if(dia>31 || dia<=0){
-        alert("Digite um Dia Válido")
-    } else if(mes>12 || mes<=0){
-        alert("Digite um Mês Válido")
-    }else{
+    } else {
         alert("Obrigado por se Inscrever!")
     }
 }
+
+
 
 
 
