56044910396d0829ac1b73c16e80a1a16c6fc510
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-06-05 19:58:55 -0300

    05.06

--- ProjectLMR/Index/scriptCadastro.js
+++ ProjectLMR/Index/scriptCadastro.js
@@ -18,6 +18,50 @@ function evento() {
     }
 }
 
+function previewFile() {
+    let preview = document.querySelector('img');
+    let file    = document.querySelector('#fAvatar').files[0];
+    let reader  = new FileReader();
+    
+    reader.onloadend = function () {
+      preview.src = reader.result;
+    }
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      
+    }
+  }
+
+function mudarConteudo() {
+    let nome = meuForm.pNome.value;
+    let evento = document.getElementById("pEvento").value;
+    let participantes = document.getElementById("pParticipantes").value;
+    let data = document.getElementById("pData").value;
+
+    let reader  = new FileReader();
+    let preview = document.querySelector('img');
+    reader.onloadend = function () {
+      preview.src = reader.result;
+}
+
+
+let card = `<div class="card" style="width: 18rem; heigth=27rem">
+<img src="${preview.src}" style="heigth: 150px" "" style= class="card-img-top" alt="...">
+<ul class="list-group list-group-flush">
+    <li class="list-group-item">${nome}</li>
+    <li class="list-group-item">${evento}</li>
+    <li class="list-group-item">${participantes}</li>
+    <li class="list-group-item">${data}</li>
+</ul>
+</div>`;
+preview.src = "";
+document.getElementById("paragrafo").innerHTML += card;
+
+
+
+}
+
 
 
 
