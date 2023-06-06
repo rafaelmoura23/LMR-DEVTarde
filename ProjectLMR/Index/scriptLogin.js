b97225ff46d4bbf1b0a883ecf3443bedc6ba0d6b
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-06-06 19:26:15 -0300

    06.06

--- ProjectLMR/Index/scriptLogin.js
+++ ProjectLMR/Index/scriptLogin.js
@@ -1,3 +1,19 @@
+function Login() {
+  let email = document.getElementById("pEmail").value;
+  let senha = document.getElementById("pSenha").value;
+  let check = document.getElementById("pCheck").value;
+
+  if (email == 0) {
+      alert("Digite o seu Email")
+  } else if (senha == 0) {
+      alert("Digite a Senha")
+  } else {
+      alert("Login Efetuado!")
+      window.location.href = "index.html";
+  }
+
+}
+
 const imageInput = document.getElementById('image-input');
     const profilePicture = document.getElementById('profile-picture');
 
